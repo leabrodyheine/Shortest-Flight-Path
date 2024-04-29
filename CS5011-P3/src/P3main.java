a773beb8b423a1caa6875b81aaaaddaa7f80f4f3
Lea <lbh5@st-andrews.ac.uk>
2024-04-29 15:29:00 +0100

    revert

--- CS5011-P3/src/P3main.java
+++ CS5011-P3/src/P3main.java
@@ -1,54 +1,79 @@
 import java.util.List;
 
+/********************
+ * Starter Code
+ * 
+ * This class contains some examples of required inputs and outputs
+ * 
+ * @author alice toniolo
+ *
+ *         run with
+ *         java P3main <Algo> <N> <ds:as> <dg:ag>
+ *         we assume <N> <ds:as> <dg:ag> are valid parameters, no need to check
+ */
+
 public class P3main {
 
 	public static void main(String[] args) {
 		if (args.length < 4) {
-			System.out.println("Usage: java P3main <DFS|BFS|AStar|BestF|SMAStar> <N> <ds:as> <dg:ag> [<param>]");
+			System.out.println("usage: java P3main <DFS|BFS|AStar|BestF|SMAStar|...> <N> <ds:as> <dg:ag> [<param>]");
 			System.exit(1);
 		}
+		// Print initial information
 		System.out.println("World: Oedipus " + args[1]);
 		System.out.println("Departure airport -- Start: " + args[2]);
 		System.out.println("Destination airport -- Goal: " + args[3]);
 		System.out.println("Search algorithm: " + args[0]);
+		System.out.println();
 
-		NodeUtility utility = new NodeUtility();
-		SearchStrategy searchStrategy = getSearchStrategy(args[0], utility);
-		if (searchStrategy == null) {
-			System.out.println("Invalid algorithm specified.");
-			return;
-		}
-
+		// Parse start and goal from args
 		String[] startParams = args[2].split(":");
 		String[] goalParams = args[3].split(":");
-		Node startNode = new Node(Integer.parseInt(startParams[0]), Integer.parseInt(startParams[1]), null, 0);
-		Node goalNode = new Node(Integer.parseInt(goalParams[0]), Integer.parseInt(goalParams[1]), null, 0);
+		PartA.Node startNode = new PartA.Node(Integer.parseInt(startParams[0]), Integer.parseInt(startParams[1]), null,
+				0);
+		PartA.Node goalNode = new PartA.Node(Integer.parseInt(goalParams[0]), Integer.parseInt(goalParams[1]), null, 0);
 
-		List<Node> path = searchStrategy.search(startNode, goalNode, Integer.parseInt(args[1]));
-		if (path != null) {
-			utility.printPath(path);
-		} else {
-			System.out.println("No path found.");
-		}
+		// Run the search algorithm
+		runSearch(args[0], Integer.parseInt(args[1]), startNode, goalNode);
 	}
 
-	private static SearchStrategy getSearchStrategy(String algo, NodeUtility util) {
-		// int[] fullDirections = { 0, 45, 90, 135, 180, 225, 270, 315 };
-		int[] restrictedDirections = { 0, 90, 180, 270 };
+	private static void runSearch(String algo, int size, PartA.Node startNode, PartA.Node goalNode) {
+		List<PartA.Node> path;
+		boolean isFinalPath = true;
 
 		switch (algo) {
 			case "BFS":
-				return new PartA.BFS(util, restrictedDirections);
+				path = PartA.bfs(startNode, goalNode, size);
+				// Calculate path cost and nodes visited
+				// Print the path if found
+				// if (path != null) {
+				// PartA.printPath(path);
+				// } else {
+				// System.out.println("No path found.");
+				// }
+				break;
 			case "DFS":
-				return new PartA.DFS(util, restrictedDirections);
-			case "BestF":
-				// return new PartB.BestFirstSearch(util, restrictedDirections);
-			case "AStar":
-				// return new AStarSearch(util);
-			case "SMAStar":
-				// return new SMAStarSearch(util);
+				path = PartA.dfs(startNode, goalNode, size);
+				// Calculate path cost and nodes visited
+				// Print the path if found
+				// if (path != null) {
+				// PartA.printPath(path);
+				// } else {
+				// System.out.println("No path found.");
+				// }
+				break;
+			case "BestF": // Placeholder for future implementation
+				// Implement Best-first search when PartB is ready
+				break;
+			case "AStar": // Placeholder for future implementation
+				// Implement A* search when PartB is ready
+				break;
+			case "SMAStar": // Placeholder for future implementation
+				// Implement SMA* search when PartB is ready
+				break;
 			default:
-				return null;
+				System.out.println("Invalid algorithm specified.");
+				break;
 		}
 	}
 }
