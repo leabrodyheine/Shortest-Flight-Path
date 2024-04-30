30f22eba87eb46015698cfa85fd99f2d27dcef14
Lea <lbh5@st-andrews.ac.uk>
2024-04-30 14:56:14 +0100

    bfs

--- CS5011-P3/src/PartA.java
+++ CS5011-P3/src/PartA.java
@@ -23,7 +23,7 @@ public class PartA {
             for (int angleChange : angleChanges) {
                 if (this.d > 0) {
                     int newAngle = (this.angle + angleChange + 360) % 360;
-                    double additionalCost = calculateAngularCost(this.d, angleChange);
+                    double additionalCost = calculateAngularCost(angleChange);
                     successors.add(new Node(this.d, newAngle, this, this.cost + additionalCost));
                 }
             }
@@ -71,12 +71,12 @@ public class PartA {
         }
     }
 
-    // private static double calculateAngularCost(int angleChange) {
-    //     return Math.abs(angleChange) / 45.0;
-    // }
-    private static double calculateAngularCost(int radius, int angleChange) {
-        return Math.abs(angleChange) * (Math.PI * radius / 4) / 45;
+    private static double calculateAngularCost(int angleChange) {
+        return Math.abs(angleChange) / 45.0;
     }
+    // private static double calculateAngularCost(int radius, int angleChange) {
+    //     return Math.abs(angleChange) * (Math.PI * radius / 4) / 45;
+    // }
     
     private static double calculateRadialCost(int distanceChange) {
         return Math.abs(distanceChange);
